c674165d255b4d661eab82764126544b3bd36eaa
nachob99 <nachoarriola99@gmail.com>
2018-01-24 10:52:18 +0100

    Implementados metodos get y setter de cada una de nuestras caracteristicas

---
+++ aulas/Aulas/aula.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class aula here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class aula
+{
+    // instance variables - replace the example below with your own
+    private String nombreAula;
+    private int numeroAula;
+    private int numeroAlumnos;
+
+    /**
+     * Constructor for objects of class aula
+     */
+    public aula()
+    {
+        // initialise instance variables
+        nombreAula="";
+        numeroAula=0;
+        numeroAlumnos=0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void añadirAula(String nombre,int numeroClase,int alumnos)
+    {
+       nombreAula=nombre;
+       numeroAula=numeroClase;
+       numeroAlumnos=alumnos;
+    }
+    
+    public int getAula(){
+        return numeroAula;
+    }
+    
+    public void setAula(int nAula){  
+        this.numeroAula=nAula;
+    }
+    
+     public int getAlumnos(){
+        return numeroAlumnos;
+    }
+    
+    public void setAlumnos(int alumnos){  
+        this.numeroAlumnos=alumnos;
+    }
+    
+    public String getNombreAula(){
+    
+        return nombreAula;
+    }
+    
+    public void setNombreAula(String nombreAula){
+        this.nombreAula=nombreAula;
+    }
+}
