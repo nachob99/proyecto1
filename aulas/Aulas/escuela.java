30074d5b3a5f57ef13059c872febe3b841263a07
nachob99 <nachoarriola99@gmail.com>
2018-01-26 10:32:52 +0100

    Creado metodo que elimina una clase

--- aulas/Aulas/escuela.java
+++ aulas/Aulas/escuela.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Collection;
 /**
  * Write a description of class escuela here.
  *
@@ -9,6 +11,7 @@ public class escuela
 {
     // instance variables - replace the example below with your own
     private ArrayList<Aula> listaAula;
+    private ArrayList<Aula> listaAulaOrdenada;
     private int numIdentificacion;
     /**
      * Constructor for objects of class escuela
@@ -23,14 +26,41 @@ public class escuela
     {
         Aula aulas = new Aula(nombre,numeroClase,alumnos,numeroIdentificativo);
         listaAula.add(aulas);
-        
+
+    }
+
+    public void listarAulas(){
+        int posicionAulas = 0;
+        while(posicionAulas < listaAula.size()){
+            System.out.println((posicionAulas+1) + "-" + listaAula.get(posicionAulas).detallesAula());
+            posicionAulas++;
+        }
+    }
+
+    public void eliminarClase(int numeroClase){
+        int cont=0;
+        while (cont < listaAula.size()) {
+            Aula numeroC = listaAula.get(cont);
+            if(numeroC.getAula()<numeroClase){
+                listaAula.remove(numeroClase);
+                cont--;
+            }
+            cont++;
+        }
+
+    }
+
+    public void modificarAlumnos(int numIdentificacion ,int nuevosAlumnos){
+        int cont=0;
+        Aula aulaActual = listaAula.get(numIdentificacion);
+        while(numIdentificacion>=0 && cont<listaAula.size()){
+            if(numIdentificacion== aulaActual.getnumeroIndentificativo()){
+                aulaActual.setAlumnos(nuevosAlumnos);
+                cont++;
+            }
+        }
     }
     
-    public void listarAulas(){
-    int posicionAulas = 0;
-        while(posicionAulas < listaAula.size()){
-        System.out.println((posicionAulas+1) + "-" + listaAula.get(posicionAulas).detallesAula());
-        posicionAulas++;
-    }
-    }
+    
 }
+
